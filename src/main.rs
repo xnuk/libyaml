ae8a5d5716c9b1de79041f43eac4523c6ab4df70
Xnuk Shuman <admin@xnu.kr>
2021-12-02 04:43:33 +0900

    rofl

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
-use yaml_rust::parser::Parser;
+use yaml_rust::parser::{Event, EventReceiver, Parser};
 
-fn main() {
-    println!("Hello, world!");
+use std::io::{BufRead, BufReader};
+use std::iter::empty;
+use yam::argf::argf;
+
+struct Recv;
+
+impl EventReceiver for Recv {
+	fn on_event(&mut self, ev: Event) {
+		println!("{:?}", ev);
+	}
+}
+
+fn main() -> anyhow::Result<()> {
+	let parser =
+		Parser::new(BufReader::new(argf()?).lines().flat_map(|line| {
+			line.unwrap_or_default()
+				.chars()
+				.chain(['\n'])
+				.collect::<Vec<_>>()
+		}))
+		.load(&mut Recv, false)?;
+
+	Ok(())
 }
